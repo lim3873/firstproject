96476dd80201fcf14205706253fea1c15ec2c00f
lim3873 <lim3873@naver.com>
2023-01-26 23:00:30 +0900

    RestController 구현

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,12 +1,12 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@Setter
 public class ArticleForm {
 
     private Long id;
