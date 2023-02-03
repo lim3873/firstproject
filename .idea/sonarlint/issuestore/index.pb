233f38553ba1c600aae191f18cfc9e9062f7afbc
lim3873 <lim3873@naver.com>
2023-02-03 20:30:30 +0900

    댓글 생성하기

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,9 +1,9 @@
 
 
 Ofirstproject/src/main/java/com/example/firstproject/api/FirstApiController.java,b\4\b4bae9ef9e2d10e3e7e2fab533b86f121e76a07c
-É
-Sfirstproject/src/main/java/com/example/firstproject/controller/FirstController.java,c\0\c0468fe33ad6dbd964d258d6b1be2a6872b867a4
-Ä
-Pfirstproject/src/main/java/com/example/firstproject/FirstprojectApplication.java,f\8\f81e21bda86665cb2ef1c06c135e5e5fe996d7a5
-Å
-Qfirstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java,0\4\049f67fcbd6cd78e57b92ab2c372424a4984e46f
+I
+firstproject/build.gradle,5\f\5f146a420ee631adbf0a95f1d82438cc66fb84a5
+â
+Yfirstproject/src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java,b\1\b1b7f88d3f4609019b736c75311988494efcff4d
+x
+Hfirstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java,0\e\0ee2646db6bb794606b986761a3c8e8f1c75da7c
--- firstproject/src/main/java/com/example/firstproject/service/CommentService.java
+++ firstproject/src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,4 +40,18 @@ public class CommentService {
                 .collect(Collectors.toList())
                 ;
     }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! 대상 게시글이 없습니."));
+        // 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+        // 댓글 엔티티를 DB로 저장
+        Comment created = commentRepository.save(comment);
+        // DTO로 변경하여 반환
+        return CommentDto.createCommentDto(created);
+    }
 }
+
